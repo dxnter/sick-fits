a8b84de150c76098301106a8209dda334696ff86
Danny Foster <contact@danfoster.io>
2018-09-28 20:22:11 -0500

    Init

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+// db connects to the remote Prisma database and provides a way to query it with JS
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
+
+module.exports = db;
---
+++ backend/src/resolvers/Mutation.js
@@ -0,0 +1,7 @@
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    console.log(args);
+  },
+};
+
+module.exports = Mutations;

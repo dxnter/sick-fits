fe3066f2c5e6a1e21dcec20d04bfe680b9baf8ee
Danny Foster <contact@danfoster.io>
2018-10-04 00:53:35 -0500

    Added signup and signin

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -61,9 +61,31 @@ const Mutations = {
     // Set the JWT as a cooke on the response
     ctx.response.cookie('token', token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+      maxAge: 1000 * 60 * 60 * 24 * 365,
     });
     // Return the user to the browser
+    return user;
+  },
+
+  async signin(parent, { email, password }, ctx, info) {
+    // Check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // Check if the password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid password');
+    }
+    // Generate the JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // Set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+    // Return the user
     return user;
   },
 };
